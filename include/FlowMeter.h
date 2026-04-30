6f25c13fcea15fe27bf67e09b4e193ff3028b93f
Learner062022 <ddylan246@yahoo.com>
2026-04-30 11:22:44 +0800

    feat(flow-meter): add pin support and getter to FlowMeter

--- include/FlowMeter.h
+++ include/FlowMeter.h
@@ -4,11 +4,14 @@
 
 class FlowMeter {
 private:
+    const uint8_t pin;
     static constexpr float pulses_per_liter = 450.0f;
     volatile unsigned long pulses = 0;
 
 public:
+    FlowMeter(uint8_t pin);
     void pulse();
     float liters() const;
     void reset();
+    uint8_t get_pin() const;
 };
